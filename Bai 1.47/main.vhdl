895d029cd42d1877ec9f35f0e6393dc602862afa
trungquyet <quyetht.ptit@gmail.com>
2021-10-21 15:38:29 +0700

    Ngan Hang Ptit

---
+++ Bai 1.47/main.vhdl
@@ -0,0 +1,39 @@
+-- viet chuong trinh mo ta VHDL cho bo dem thap phan tien 2 digit cos reset khong dong bo?
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity main is
+    port(
+        clk, res : in std_logic;
+        chuc, dvi: out std_logic_vector (3 downto 0)
+    );
+    end main;
+
+architecture behavi of main is
+    signal q1, q2 : std_logic_vector := "0000";
+    begin
+        process(clk, res)
+        begin
+            if(res <= '1') then
+                q1 <= "0000";
+                q2 <= "0000";
+            else
+                if(rising_edge(clk)) then
+                    if(q2 <= "1001") then
+                        q2 <= "0000";
+                        if(q1 <= "1001") then
+                            q1 <= "0000";
+                        else
+                            q1 <= q1 + 1;
+                        end if;
+                    else
+                        q2 <= q2 + 1;
+                    end if;
+                end if;
+            end if;
+        end process;
+
+    chuc <= q1;
+    dvi  <= q2;
+    end behavi;
---
+++ Bai 1.47/test bench.vhdl
@@ -0,0 +1,41 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity test_bench is
+end test_bench;
+
+architecture behavi of test_bench is
+    component main is
+        port(
+            clk, res: in  std_logic;
+            chuc, dvi:out std_logic_vector(3 downto 0)
+        );
+        end component;
+    signal clk, res : std_logic;
+    signal chuc, dvi: std_logic_vector(3 downto 0);
+    begin
+        HTQ: main port map (
+            clk  => clk,
+            res  => res,
+            chuc => chuc,
+            dvi  => dvi
+        );
+
+        clock:process
+        begin
+            clk <= '1';
+            wait for 20ns;
+            clk <= '0';
+            wait for 20ns;
+        end process clock;
+
+        test: process
+        begin
+            res <= '1';
+            wait for 50ns;
+            res <= '0';
+            wait for 100ns;
+        end process test;
+
+    end behavi;
