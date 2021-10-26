faf24ac0d1f1068b07e2888ab040a49308bbc396
trungquyet <quyetht.ptit@gmail.com>
2021-10-26 15:48:52 +0700

    Ngan hang PIT

---
+++ Bai 2.9/main.vhdl
@@ -0,0 +1,15 @@
+--viet chuong trinh mo ta VHDL cho mach giai ma nhi phan sang thap phan, sang gray va nguoc lai?
+
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+entity main is
+    port(
+        
+    );
+    end main;
+
+architecture behavi of main is
+    begin
+
+    end behavi;
